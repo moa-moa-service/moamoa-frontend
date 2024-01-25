f9ef009b7ae14f39ef13e9bd7f967d43a0b71502
seoh77 <seoh7dev@gmail.com>
2024-01-25 23:11:45 +0900

    feat: 검색페이지 인기 검색어 및 최근 검색어 디자인 구현

--- src/app/SearchPage/SearchPage.main.jsx
+++ src/app/SearchPage/SearchPage.main.jsx
@@ -12,8 +12,27 @@ function SearchPageMain() {
                     <itemS.KeywordTitle>우리 동네 인기 검색어</itemS.KeywordTitle>
                     <itemS.TopKeywordContainer>
                         <itemS.TopKeyword>캘린더</itemS.TopKeyword>
+                        <itemS.TopKeyword>보조배터리</itemS.TopKeyword>
+                        <itemS.TopKeyword>다이어리</itemS.TopKeyword>
+                        <itemS.TopKeyword>행거</itemS.TopKeyword>
+                        <itemS.TopKeyword>김치</itemS.TopKeyword>
                     </itemS.TopKeywordContainer>
-                    <itemS.KeywordTitle>최근 검색어</itemS.KeywordTitle>
+                    <itemS.KeywordTitle>
+                        최근 검색어
+                        <itemS.deleteText>전체 삭제</itemS.deleteText>
+                    </itemS.KeywordTitle>
+                    <itemS.RecentKeywordContainer>
+                        <itemS.RecentKeyword>2024년 달력</itemS.RecentKeyword>
+                        <itemS.RecentKeywordDelete>X</itemS.RecentKeywordDelete>
+                    </itemS.RecentKeywordContainer>
+                    <itemS.RecentKeywordContainer>
+                        <itemS.RecentKeyword>올리브유</itemS.RecentKeyword>
+                        <itemS.RecentKeywordDelete>X</itemS.RecentKeywordDelete>
+                    </itemS.RecentKeywordContainer>
+                    <itemS.RecentKeywordContainer>
+                        <itemS.RecentKeyword>텀블러</itemS.RecentKeyword>
+                        <itemS.RecentKeywordDelete>X</itemS.RecentKeywordDelete>
+                    </itemS.RecentKeywordContainer>
                 </itemS.KeywordContainer>
             </itemS.SearchPageContainer>
         </>
--- src/app/SearchPage/styled/SearchPage.main.style.js
+++ src/app/SearchPage/styled/SearchPage.main.style.js
@@ -46,21 +46,55 @@ export const KeywordContainer = styled.div`
 `
 
 export const KeywordTitle = styled.div`
-    margin-top: 25px;
+    margin: 25px 0 20px 0;
     font-size: 18px;
     font-weight: 600;
     color: #000000;
+    display: flex;
+    justify-content: space-between;
 `
 
 export const TopKeywordContainer = styled.div`
     display: flex;
-    margin: 15px 0;
+    margin-bottom: 10px;
+    white-space: nowrap;
+    overflow-x: auto;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
 `
 
 export const TopKeyword = styled.div`
+    width: auto;
+    height: 15px;
     margin-right: 10px;
     padding: 7px 18px;
     border-radius: 20px;
     border: 1px solid #2B4760;
     color: #2B4760;
+`
+
+export const deleteText = styled.div`
+    color: #888888;
+    font-size: 12px;
+`
+
+export const RecentKeywordContainer = styled.li`
+    display: flex;
+    justify-content: space-between;
+    margin: 5px 10px;
+    border-bottom: 0.8px solid #E4E4E4;
+    padding: 3px 7px 13px 7px;
+`
+
+export const RecentKeyword = styled.div`
+    color: #2B4760;
+    font-size: 15px;
+`
+
+export const RecentKeywordDelete = styled.div`
+    width: 15px;
+    height: 15px;
+    color: #D2D2D2;
 `
