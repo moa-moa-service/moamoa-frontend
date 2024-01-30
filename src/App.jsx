250929f1365c0f2e6db0bd053de85eeece6f91ad
HanaPark1 <ydjxlst@gmail.com>
2024-01-30 13:56:48 +0900

    feat: 네이버 지도 웹에 출력

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,29 @@ import LoginPageComponent from './app/LoginPage/LoginPage.main.jsx'
 import RecruitmentPage from './app/RecruitmentPage/Recruitment.main.jsx'
 import SearchPageMain from './app/SearchPage/SearchPage.main.jsx'
 import ProductPage from './app/ProductPage/ProductPage.main.jsx'
-
+import MyMap from './app/RecruitmentPage/Recruitment.MyMap.jsx'
 import {
   Route,
   Routes,
 } from "react-router-dom";
+import { NavermapsProvider } from 'react-naver-maps'
 
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path='/detail' element={<CategoryDetailPage />} />
-        <Route path="/login" element={<LoginPageComponent />} />
-        <Route path="/recruitment" element={<RecruitmentPage />} />
-        <Route path="/search" element={<SearchPageMain />} />
-        <Route path="/product" element={<ProductPage />} />
-      </Routes>
+      <NavermapsProvider ncpClientId='%import.meta.env.VITE_CLIENT_ID%'>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path='/detail' element={<CategoryDetailPage />} />
+          <Route path="/login" element={<LoginPageComponent />} />
+          <Route path="/recruitment" element={<RecruitmentPage />} />
+          <Route path="/search" element={<SearchPageMain />} />
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/mymap" element={<MyMap />} />
+        </Routes>
+      </NavermapsProvider>
     </div>
   )
 }
