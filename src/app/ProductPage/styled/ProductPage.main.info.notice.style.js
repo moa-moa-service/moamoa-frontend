87efa86cb6e85622d22bdc9f44094eb9859f69c6
seoh77 <seoh7dev@gmail.com>
2024-02-05 13:57:07 +0900

    feat: 상품페이지 공지사항 부분 추가

---
+++ src/app/ProductPage/styled/ProductPage.main.info.notice.style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const NoticeContainer = styled.div`
+    height: 55px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const Notice = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Title = styled.div`
+    font-size: 15px;
+    color: #2B4760;
+    font-weight: 600;
+    margin-bottom: 7px;
+`
+
+export const Content = styled.div`
+    color: #888888;
+    font-size: 11px;
+`
+
+export const Day = styled.div`
+    color: #888888;
+    font-size: 12px;
+`
