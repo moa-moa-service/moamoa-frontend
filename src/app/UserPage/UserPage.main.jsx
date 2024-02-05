c9eb994f034357d198a86a33e6978e12268b57de
YU-JIN-JUNG <yuuuujiiiin@naver.com>
2024-02-05 18:59:02 +0900

    feat: 타사용자페이지 퍼블리싱

---
+++ src/app/UserPage/UserPage.main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Recruting from './UserPage.main.Recruting'
+
+const UserPageComponent = () => {
+    // const [step, setStep] = useState(0)
+    // 0 - LoginWelcome
+    // 1 - Basic Information
+
+    return (
+        /*
+        <LoginPageContainer>
+            (
+                if (step === 0) {
+                    <LoginWelcome />
+                } else if (step === 1) {
+                    <BasicInformation />
+                }
+            )
+        </LoginPageContainer>
+        */
+       <>
+        <Recruting />
+       </>
+    )
+}
+
+export default UserPageComponent
