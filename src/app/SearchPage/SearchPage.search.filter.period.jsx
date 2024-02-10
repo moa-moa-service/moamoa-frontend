91a13d2ba02326fb421194066afce1685ebe383f
seoh77 <seoh7dev@gmail.com>
2024-02-10 20:42:40 +0900

    feat: 검색페이지 모집 기간 필터 화면 캘린더 구현

--- src/app/SearchPage/SearchPage.search.filter.period.jsx
+++ src/app/SearchPage/SearchPage.search.filter.period.jsx
@@ -1,7 +1,45 @@
 import * as C from "./styled/SearchPage.search.filter.style"
 import * as itemS from "./styled/SearchPage.search.filter.period.style"
+import { useState } from "react"
+import { format, addDays, endOfMonth, endOfWeek, startOfMonth, startOfWeek, isSameMonth, isSameDay } from "date-fns" 
 
 function FilterPeriod({openPeriodFilter}) {
+
+    const currentDate = new Date() ;   // 현재 날짜
+
+    const startDate = startOfMonth(currentDate) ;   // 현재 월의 시작일
+    const endDate = endOfMonth(currentDate) ;       // 현재 월의 마지막일
+
+    const startWeek = startOfWeek(startDate) ;      // 현재 월의 첫 주
+    const endWeek = endOfWeek(endDate) ;            // 현재 월의 마지막 주
+
+    // 달력에 표시할 날짜 배열 생성
+    const rows = [] ;
+    let days = [] ;
+    let day = startWeek ;
+
+    while(day <= endWeek) {
+        for(let i=0; i<7; i++) {
+
+            const color = isSameMonth(day, currentDate) ? null : 'gray' ;
+            const checkday = isSameDay(day, Date()) ? 'today' : null ;
+
+            days.push(
+                <itemS.day key={day}>
+                    <itemS.dayText color={color} checkday={checkday}>{format(day,'d')}</itemS.dayText>
+                    {checkday === 'today' && <itemS.todayText>Today</itemS.todayText> } 
+                </itemS.day>
+            ) ;
+            day = addDays(day, 1) ;
+        }
+        rows.push(
+            <itemS.days key={day}>
+                {days}
+            </itemS.days>
+        ) ;
+        days = [] ;
+    }
+
     return(
         <>
         <C.FilterContainer>
@@ -15,117 +53,22 @@ function FilterPeriod({openPeriodFilter}) {
                         <itemS.btn>
                             <img src="../../../public/SearchPage/angle_left.png" />
                         </itemS.btn>
-                        <itemS.MonthText>2023년 7월</itemS.MonthText>
+                        <itemS.MonthText>{format(currentDate, "yyyy년 M월")}</itemS.MonthText>
                         <itemS.btn>
                             <img src="../../../public/SearchPage/angle_right.png" />
                         </itemS.btn>
                     </itemS.CalendarNav>
                     <itemS.Calendar>
                         <itemS.weeks>
+                            <itemS.week>일</itemS.week>
                             <itemS.week>월</itemS.week>
                             <itemS.week>화</itemS.week>
                             <itemS.week>수</itemS.week>
                             <itemS.week>목</itemS.week>
                             <itemS.week>금</itemS.week>
                             <itemS.week>토</itemS.week>
-                            <itemS.week>일</itemS.week>
                         </itemS.weeks>
-                        <itemS.days>
-                            <itemS.day>
-                                <itemS.dayText>1</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>2</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>3</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>4</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>5</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText color="gray">6</itemS.dayText>
-                                <itemS.todayText>Today</itemS.todayText>    
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText color="red">7</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>8</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>9</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>10</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>11</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>12</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>13</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>14</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>15</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>16</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>17</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>18</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>19</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>20</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>21</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>22</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>23</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>24</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>25</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>26</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>27</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>28</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>29</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>30</itemS.dayText>
-                            </itemS.day>
-                            <itemS.day>
-                                <itemS.dayText>31</itemS.dayText>
-                            </itemS.day>
-                        </itemS.days>
+                        {rows}
                     </itemS.Calendar>
                 </itemS.CalendarContainer>
                 <C.BtnContainer>
