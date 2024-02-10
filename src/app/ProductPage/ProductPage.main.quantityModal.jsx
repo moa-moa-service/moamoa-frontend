d003b2874e93f5a252e1e30f8c2bf2a3bb4b608b
seoh77 <seoh7dev@gmail.com>
2024-02-10 11:06:51 +0900

    feat: 상품페이지 상품 수량 선택 취소 버튼 기능 구현

--- src/app/ProductPage/ProductPage.main.quantityModal.jsx
+++ src/app/ProductPage/ProductPage.main.quantityModal.jsx
@@ -3,7 +3,7 @@ import * as itemS from "./styled/ProductPage.main.selectModalComponent.style"
 function QuantityModal({openQuantityModalHandler}) {
     return(
         <>
-            <itemS.SelectModalContainer onClick={openQuantityModalHandler}>
+            <itemS.SelectModalContainer>
                 <itemS.SelectModal onClick={(e) => e.stopPropagation()}>
                     <itemS.Icon>
                         <img src="../../../public/ProductPage/shopping_cart.png" alt="장바구니" />
@@ -11,7 +11,7 @@ function QuantityModal({openQuantityModalHandler}) {
                     <itemS.InfoTitle>상품 수량을 선택해주세요!</itemS.InfoTitle>
                     <itemS.TextInput placeholder="상품 개수 입력 필드"></itemS.TextInput>
                     <itemS.BtnContainer>
-                        <itemS.CheckBtn>아니오</itemS.CheckBtn>
+                        <itemS.CheckBtn onClick={openQuantityModalHandler}>아니오</itemS.CheckBtn>
                         <itemS.CheckBtn color="red">네</itemS.CheckBtn>
                     </itemS.BtnContainer>
                 </itemS.SelectModal>
