c37fae93ae76f5f14ad69155cbd25e9bb0b61d1b
YU-JIN-JUNG <yuuuujiiiin@naver.com>
2024-02-12 20:50:12 +0900

    feat: client 객체에 token이 없을 때 분기 추가

--- src/client.js
+++ src/client.js
@@ -1,15 +1,28 @@
 import axios from "axios";
 
-const client = (token) => axios.create({
-    baseURL: import.meta.env.VITE_ENDPOINT,
-    headers: {
-        "Content-Type" : `applicaiton/json;charset=UTF-8`,
-        "Accept" : "application/json",
-        "Authorization": "Bearer " + token,
+const client = (token) => {
+    const ENDPOINT = import.meta.env.VITE_ENDPOINT
 
-        "Access-Control-Allow-Origin": `http://localhost:5173`,
-        "Access-Control-Allow-Credentials" : "true",
+    if (!ENDPOINT) {
+        console.error('Endpoint is not exist')
+        return
     }
-}) ;
+    
+    const config = {
+        baseURL: ENDPOINT,
+        headers: {
+            "Content-Type" : `applicaiton/json;charset=UTF-8`,
+            "Accept" : "application/json",
+            "Access-Control-Allow-Origin": `http://localhost:5173`,
+            'Access-Control-Allow-Credentials':"true",
+        }  
+    }
+
+    if (token) {
+        config["headers"]["Authorization"] = "Bearer " + token
+    }
+
+    return axios.create(config)
+}
 
 export default client
