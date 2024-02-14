d8e4ef26097cfdb045dc606aaa607ef91cce56e7
seoh77 <seoh7dev@gmail.com>
2024-02-15 00:20:07 +0900

    design: 참여 완료 모달 안내 문구 변경

--- src/app/ProductPage/ProductPage.main.completeModal.jsx
+++ src/app/ProductPage/ProductPage.main.completeModal.jsx
@@ -18,7 +18,7 @@ function CompleteModal({openCompleteModalHandler}) {
                     <img src="../../../public/ProductPage/check.png" alt="체크 표시" />
                 </itemS.NoticeIcon>
                 <itemS.NoticeText>참여가 완료되었어요!</itemS.NoticeText>
-                <itemS.NoticeText size='small'>모집이 완료되면 자동으로 채팅방이 개설되어요!</itemS.NoticeText>
+                {/* <itemS.NoticeText size='small'>모집이 완료되면 자동으로 채팅방이 개설되어요!</itemS.NoticeText> */}
             </itemS.NoticeModalContainer>
         </>
     )
