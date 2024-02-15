1feb9b318cc17b57ec904879374382c12fc68fbb
HanaPark1 <ydjxlst@gmail.com>
2024-02-15 22:04:16 +0900

    feat: 메인 페이지 각 카테고리별 조회 api 호출 기능 추가

--- src/app/MainPage/MainPage.main.CategoryList.Category.Item.jsx
+++ src/app/MainPage/MainPage.main.CategoryList.Category.Item.jsx
@@ -1,19 +1,22 @@
 import * as itemS from "./styled/MainPage.main.CategoryList.Category.Item.style"
 
-function Item() {
-    return(
+function Item(props) {
+    return (
         <>
             <itemS.ItemWrapper>
                 <itemS.ItemContainer>
-                <itemS.ItemImg>
-                    <itemS.ItemRecruitContainer>
-                        <itemS.ItemRecruitText type='People'>모집 5명</itemS.ItemRecruitText>
-                        <itemS.ItemRecruitText type='Deadline'>D-3</itemS.ItemRecruitText>
-                    </itemS.ItemRecruitContainer>
-                </itemS.ItemImg>
-                <itemS.ItemName>헤어드라이기</itemS.ItemName>
-                <itemS.ItemPrice>13000원</itemS.ItemPrice>
-            </itemS.ItemContainer>
+                    <itemS.ItemImgContainer>
+                        <itemS.ItemImg src={props.item.imageUrl} />
+                        <itemS.ItemRecruitContainer>
+                            <itemS.ItemRecruitTextContainer>
+                                <itemS.ItemRecruitText type='People'>모집 {props.item.personnel}명</itemS.ItemRecruitText>
+                                <itemS.ItemRecruitText type='Deadline'>D-{props.item.dDay}</itemS.ItemRecruitText>
+                            </itemS.ItemRecruitTextContainer>
+                        </itemS.ItemRecruitContainer>
+                    </itemS.ItemImgContainer>
+                    <itemS.ItemName>{props.item.productName}</itemS.ItemName>
+                    <itemS.ItemPrice>{props.item.price}원</itemS.ItemPrice>
+                </itemS.ItemContainer>
             </itemS.ItemWrapper>
         </>
     )
