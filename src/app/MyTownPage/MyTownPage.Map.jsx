9e7100ef63c6160fad772e1d1e70957ec49a7b53
HanaPark1 <ydjxlst@gmail.com>
2024-02-16 02:50:36 +0900

    feat: 우리 동네 api 호출

---
+++ src/app/MyTownPage/MyTownPage.Map.jsx
@@ -0,0 +1,96 @@
+import { Container as MapDiv, NaverMap, Marker, useNavermaps } from 'react-naver-maps';
+import { useState, useEffect, useRef } from 'react';
+
+import client from '../../client';
+import BottomWrapper from './MyTownPage.main.BottomWrapper.jsx';
+
+function Map() {
+    const navermaps = useNavermaps();
+    const mapRef = useRef(null);
+
+    const [isMapLoading, setIsMapLoading] = useState(true);
+    const [myLocation, setMyLocation] = useState({ lat: null, lng: null });
+
+    const [products, setProducts] = useState();
+    const [selectedProduct, setSelectedProduct] = useState();
+
+    const handleMarkerClick = (product) => {
+        setSelectedProduct(product);
+    };
+
+    useEffect(() => {
+        setIsMapLoading(true);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+        function success(position) {
+            setMyLocation({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+            setIsMapLoading(false);
+        }
+        function error() {
+            console.log('사용자 위치 불러오기 실패');
+            setMyLocation({ lat: 37.4979517, lng: 127.0276188 });
+            setIsMapLoading(false);
+        }
+
+        const auth = import.meta.env.VITE_AUTH;
+        const fetchData = async () => {
+            try {
+                const response = await client(auth).get(
+                    '/posts/near'
+                );
+                setProducts(response.data.result.SimplePostDtoList);
+            } catch (error) {
+                console.error('안된다!!:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+
+
+    return (
+        <MapDiv style={{
+            width: '100%',
+            height: '80vh',
+        }}>
+            {isMapLoading ? (
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%',
+                    width: '100%',
+                }}>Loading...</div>
+            ) : (<>
+                <NaverMap
+                    ref={mapRef}
+                    defaultCenter={new navermaps.LatLng(myLocation.lat, myLocation.lng)}
+                    defaultZoom={18}
+                >
+                    {myLocation.lat && myLocation.lng && (
+                        <Marker
+                            position={new navermaps.LatLng(myLocation.lat, myLocation.lng)}
+                        />
+                    )}
+                    {products && products.map(product => (
+                        <Marker
+                            onClick={() => handleMarkerClick(product)}
+                            position={new navermaps.LatLng(product.address.latitude, product.address.longitude)}
+                        />
+                    ))}
+                    {selectedProduct && (
+                    <BottomWrapper product={selectedProduct} />
+                )}
+                </NaverMap>
+            </>
+
+            )}
+        </MapDiv>
+    );
+}
+
+export default Map;
