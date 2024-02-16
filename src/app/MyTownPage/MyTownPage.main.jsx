c23f41bbfc36dfd93ad488c159da52a0455577dd
HanaPark1 <ydjxlst@gmail.com>
2024-02-16 18:26:11 +0900

    feat: 우리동네 동네명 지도 api 호출 후 UI 표시

--- src/app/MyTownPage/MyTownPage.main.jsx
+++ src/app/MyTownPage/MyTownPage.main.jsx
@@ -2,12 +2,17 @@ import * as itemRec from '../RecruitmentPage/styled/Recruitment.TradingLocation.
 import Map from './MyTownPage.Map.jsx';
 import * as itemS from './styled/MyTownPage.main.style.js'
 
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
 function MyTown() {
     const navigate = useNavigate();
+    const [town, setTown] = useState('');
+
+    const handleTownChange = (data) => {
+        setTown(data);
+    };
 
     return (
         <>
@@ -16,12 +21,12 @@ function MyTown() {
                     <itemRec.BackBtn type='town' onClick={() => (navigate(-1))} />
                     <itemS.TopTextContainer>
                         <itemS.TopText>우리 동네 공동 구매 찾기</itemS.TopText>
-                        <itemS.TopText type='bold'>남가좌동</itemS.TopText>
+                        <itemS.TopText type='bold'>{town}</itemS.TopText>
                     </itemS.TopTextContainer>
                 </itemRec.TopContentContainer>
             </itemRec.TopWrapper>
             <Suspense fallback={null}>
-                <Map/>
+                <Map onTown={handleTownChange}/>
             </Suspense>
         </>
     )
