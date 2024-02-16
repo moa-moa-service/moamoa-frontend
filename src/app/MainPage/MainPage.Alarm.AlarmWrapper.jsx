af66ce13ee5ee2b77f3d6dff756ba4a89e46f680
HanaPark1 <ydjxlst@gmail.com>
2024-02-17 03:33:49 +0900

    feat: 알림 페이지 api 호출

--- src/app/MainPage/MainPage.Alarm.AlarmWrapper.jsx
+++ src/app/MainPage/MainPage.Alarm.AlarmWrapper.jsx
@@ -1,20 +1,64 @@
+import { useEffect, useState } from "react";
 import * as itemS from "./styled/MainPage.Alarm.style"
-import * as itemDetail from './styled/MainPage.CategoryDatailPage.style'
+import Cart from '../../../public/MainPage/cart.png'
+import Comment from '../../../public/MainPage/comment.png'
+import Speaker from '../../../public/MainPage/speaker.png'
 
 
-function AlarmWrapper() {
+function AlarmWrapper(alarm) {
+    const [time, setTime] = useState();
+
+
+    useEffect(() => {
+        const currentTime = new Date();
+        const createdTime = new Date(alarm.item.createdAt);
+        console.log('alarm.item.createdAt',alarm.item.createdAt);
+
+        const getTimeDifference = (currentTime, createdTime) => {
+            const differenceInMilliseconds = Math.abs(currentTime - createdTime);
+
+            const seconds = Math.floor(differenceInMilliseconds / 1000);
+            const minutes = Math.floor(seconds / 60);
+            const hours = Math.floor(minutes / 60);
+            const days = Math.floor(hours / 24);
+            const weeks = Math.floor(days / 7);
+            const months = Math.floor(days / 30);
+
+            if (months >= 1) {
+                return `${months}달 전`;
+            } else if (weeks >= 1) {
+                return `${weeks}주 전`;
+            } else if (days >= 1) {
+                return `${days}일 전`;
+            } else if (hours >= 1) {
+                return `${hours}시간 전`;
+            } else if (minutes >= 1) {
+                return `${minutes}분 전`;
+            } else {
+                return '방금 전';
+            }
+        }
+        const timeDifference = getTimeDifference(currentTime, createdTime);
+        setTime(timeDifference);
+    }, [])
+
     return (
         <>
             <itemS.AlarmWrapper>
-                <itemDetail.BackBtn />
+                {alarm.item.type === '공동구매' ? (
+                    <itemS.AlarmImg src={Cart} />
+                ) : alarm.item.type === '공지사항' ? (
+                    <itemS.AlarmImg src={Speaker} />
+                ) : (
+                    <itemS.AlarmImg src={Comment} />
+                )}
                 <itemS.AlarmTextContainer>
                     <itemS.TextContainer>
-                        <itemS.Text>댓글</itemS.Text>
-                        <itemS.Text>3분 전</itemS.Text>
+                        <itemS.Text>{alarm.item.type}</itemS.Text>
+                        <itemS.Text>{time}</itemS.Text>
                     </itemS.TextContainer>
                     <itemS.TextContainer type='content'>
-                        <itemS.Text type='bold'>양키</itemS.Text>
-                        <itemS.Text type='content'>님이 공지사항 제목에 댓글을 달았어요!</itemS.Text>
+                        <itemS.Text type='content'>{alarm.item.message}</itemS.Text>
                     </itemS.TextContainer>
                 </itemS.AlarmTextContainer>
             </itemS.AlarmWrapper>
--- src/app/MainPage/MainPage.Alarm.jsx
+++ src/app/MainPage/MainPage.Alarm.jsx
@@ -4,22 +4,42 @@ import * as itemCategory from "./styled/MainPage.main.CategoryList.style"
 import AlarmWrapper from './MainPage.Alarm.AlarmWrapper'
 import BackIcon from '../../../public/SearchPage/backIcon.png'
 
+import client from '../../client'
+
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function Alarm() {
     const navigate = useNavigate();
-    return(
+    const [alarms, setAlarms] = useState();
+
+    useEffect(() => {
+        const auth = import.meta.env.VITE_AUTH;
+        const fetchData = async () => {
+            try {
+                const response = await client(auth).get(
+                    '/notifications'
+                );
+                setAlarms(response.data.result.notificationDTOList);
+            } catch (error) {
+                console.error('안된다!!:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
         <>
             <itemMain.MainPageContainer>
                 <itemDetail.CategoryTitleContainer type='alarm'>
-                    <itemDetail.BackBtn src={BackIcon} onClick={() => { navigate(-1); }}/>
+                    <itemDetail.BackBtn src={BackIcon} onClick={() => { navigate(-1); }} />
                     <itemDetail.CategoryTitle>알림</itemDetail.CategoryTitle>
                 </itemDetail.CategoryTitleContainer>
-            <itemCategory.CategoryListContainer>
-                <AlarmWrapper />
-                <itemDetail.ContourLine />
-                <AlarmWrapper />
-            </itemCategory.CategoryListContainer>
+                <itemCategory.CategoryListContainer >
+                    {alarms && alarms.map(alarm => (
+                        <AlarmWrapper key={alarm.referenceId} item={alarm} />
+                    ))}
+                </itemCategory.CategoryListContainer >
             </itemMain.MainPageContainer>
         </>
     )
