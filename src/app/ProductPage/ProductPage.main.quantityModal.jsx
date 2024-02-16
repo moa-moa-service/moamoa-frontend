1fb41aa16cdce8b88a0cb5cf4af44422845443f6
seoh77 <seoh7dev@gmail.com>
2024-02-16 20:19:25 +0900

    feat: 상품페이지 참여하기 기능 구현

--- src/app/ProductPage/ProductPage.main.quantityModal.jsx
+++ src/app/ProductPage/ProductPage.main.quantityModal.jsx
@@ -4,14 +4,30 @@ import * as itemS from "./styled/ProductPage.main.selectModalComponent.style"
 
 function QuantityModal({openQuantityModalHandler, id, openCompleteModalHandler}) {
 
+    const [quantityInput, setQuantityInput] = useState('') ;
+
+    const inputHandle = (e) => {
+        setQuantityInput(e.target.value) ;
+    }
+
     const joinHandle = async() => {
         const auth = import.meta.env.VITE_AUTH ;
     
+        if(!quantityInput || isNaN(quantityInput)) {
+            console.log("수량이 입력되지 않았습니다.")
+            return ;
+        }
+
         try{
-            // const response = await client(auth).post(`/posts/${id}/join`) ;
+            const response = await client(auth).post(`/posts/${id}/join`, {
+                amount : parseInt(quantityInput) ,
+            }, {
+                headers : {
+                    "Content-Type" : "application/json" ,
+                }
+            }) ;
             openQuantityModalHandler() ;
             openCompleteModalHandler() ;
-            // console.log("성공" + response) ;
         } catch (error) {
             console.error("실패" + error) ;
         }
@@ -25,7 +41,7 @@ function QuantityModal({openQuantityModalHandler, id, openCompleteModalHandler})
                         <img src="../../../public/ProductPage/shopping_cart.png" alt="장바구니" />
                     </itemS.Icon>
                     <itemS.InfoTitle>상품 수량을 선택해주세요!</itemS.InfoTitle>
-                    <itemS.TextInput placeholder="상품 개수 입력 필드"></itemS.TextInput>
+                    <itemS.TextInput placeholder="상품 개수 입력 필드" onChange={inputHandle}></itemS.TextInput>
                     <itemS.BtnContainer>
                         <itemS.CheckBtn onClick={openQuantityModalHandler}>취소</itemS.CheckBtn>
                         <itemS.CheckBtn color="red" onClick={joinHandle}>확인</itemS.CheckBtn>
