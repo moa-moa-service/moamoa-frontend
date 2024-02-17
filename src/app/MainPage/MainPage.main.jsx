4c9ef5f8173ff869384333fe9d9fbc9e9163b92f
YU-JIN-JUNG <yuuuujiiiin@naver.com>
2024-02-18 00:11:40 +0900

    feat: 추가정보 입력 후 메인페이지로 리다이렉트 되었을 때 authorization param이 남아 있으면 제거한다

--- src/app/MainPage/MainPage.main.jsx
+++ src/app/MainPage/MainPage.main.jsx
@@ -3,14 +3,25 @@ import * as itemS from "./styled/MainPage.main.style"
 import CategoryList from "./MainPage.main.CategoryList"
 import MenuNav from "../MenuNav/MenuNav.main"
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 import { AuthAtom } from '../../recoil/atoms/AuthAtom'
 import { useRecoilState } from 'recoil'
+import { useEffect } from 'react'
 
 function MainPage() {
     const navigate = useNavigate()
-    const [accessToken] = useRecoilState(AuthAtom)
-    console.log(accessToken)
+    const [_, setAccessToken] = useRecoilState(AuthAtom)
+
+    const location = useLocation()
+    const params = new URLSearchParams(location.search)
+    
+    useEffect(() => {
+        const _accessToken = params.get('authorization')
+        if (_accessToken) {
+            setAccessToken(_accessToken)
+            params.delete('authorization')
+        }
+    }, [])
     
     return (
         <>
